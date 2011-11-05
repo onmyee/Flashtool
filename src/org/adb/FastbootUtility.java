6a023b71ce0cb6ce2322fcf914b64b48f0000c72
Androxyde <emmanuel.odille@gmail.com>
2011-11-05 15:24:46 +0100

    cosmetics

--- src/org/adb/FastbootUtility.java
+++ src/org/adb/FastbootUtility.java
@@ -7,12 +7,12 @@ public class FastbootUtility {
 	
 	private static String fsep = OS.getFileSeparator();
 	
-	public static void AdbRebootFastboot() throws Exception {
+	public static void adbRebootFastboot() throws Exception {
 		OsRun command = new OsRun("."+fsep+"x10flasher_lib"+fsep+"adb reboot bootloader");
 		command.run();
 	}
 	
-	public static void hotboot(String bootimg) throws Exception {
+	public static void hotBoot(String bootimg) throws Exception {
 		OsRun command = new OsRun("."+fsep+"x10flasher_lib"+fsep+"fastboot boot "+bootimg);
 		command.run();		
 	}
