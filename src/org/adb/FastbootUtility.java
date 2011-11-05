fffa0e81805ddd784fd5742892e7958de14d7a03
DooMLoRD <metoo.mememe@gmail.com>
2011-11-05 21:12:56 +0530

    additions to fastboot API

--- src/org/adb/FastbootUtility.java
+++ src/org/adb/FastbootUtility.java
@@ -16,5 +16,34 @@ public class FastbootUtility {
 		OsRun command = new OsRun("."+fsep+"x10flasher_lib"+fsep+"fastboot boot "+bootimg);
 		command.run();		
 	}
-
+	
+	public static void flashBoot(String bootimg) throws Exception {
+		OsRun command = new OsRun("."+fsep+"x10flasher_lib"+fsep+"fastboot flash boot "+bootimg);
+		command.run();		
+	}
+	
+	public static void rebootDevice() throws Exception {
+		OsRun command = new OsRun("."+fsep+"x10flasher_lib"+fsep+"fastboot reboot");
+		command.run();
+	}
+	
+	public static void rebootFastboot() throws Exception {
+		OsRun command = new OsRun("."+fsep+"x10flasher_lib"+fsep+"fastboot reboot-bootloader");
+		command.run();
+	}
+	
+	public static void wipeDataCache() throws Exception {
+		OsRun command = new OsRun("."+fsep+"x10flasher_lib"+fsep+"fastboot -w");
+		command.run();
+	}
+	
+	public static void getDeviceInfo() throws Exception {
+		OsRun command = new OsRun("."+fsep+"x10flasher_lib"+fsep+"fastboot devices");
+		command.run();
+	}
+	
+	public static void getFastbootVerInfo() throws Exception {
+		OsRun command = new OsRun("."+fsep+"x10flasher_lib"+fsep+"fastboot.exe -i 0x0fce getvar version");
+		command.run();
+	}
 }
